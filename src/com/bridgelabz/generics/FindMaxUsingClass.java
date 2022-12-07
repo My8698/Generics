01e48b0e836be2078faa3b49fa82089e971eefaa
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-07 21:48:38 +0530

    Added:write printMax generics method and internally call it from maximum method

--- src/com/bridgelabz/generics/FindMaxUsingClass.java
+++ src/com/bridgelabz/generics/FindMaxUsingClass.java
@@ -14,6 +14,9 @@ public class FindMaxUsingClass<T extends Comparable <T>> {
         this.number3 = number3;
         this.number4 = number4;
     }
+    public T maximum(){
+        return FindMaxUsingClass.maxOfValues(number1, number2, number3, number4);
+    }
     /**
      * static method maxOfValues by passing three instance variables
      */
@@ -27,9 +30,23 @@ public class FindMaxUsingClass<T extends Comparable <T>> {
             max = number4;
         return max;
     }
+
+    /**
+     *Generic method to printMax to print the maximum values
+     */
+    public static <T> void printMax(T number) {
+        System.out.println("Maximum value is : "+ number);
+    }
     public static void main(String[] args) {
-        System.out.println("The maximum value between the three Integer is : " + maxOfValues (2, 25, 50,65)+ "\n");
+       /** System.out.println("The maximum value between the three Integer is : " + maxOfValues (2, 25, 50,65)+ "\n");
         System.out.println("The maximum value between the three Float is : " + maxOfValues (2.7f, 25.5f, 50.9f,66.1f)+ "\n");
         System.out.println("The maximum value between the three String is : " + maxOfValues ("apple", "peach", "banana","guava"));
-    }
+    */
+        FindMaxUsingClass<Integer> maxInteger = new FindMaxUsingClass<Integer>(2, 25, 50, 60);
+        System.out.println("The maximum value between the three Integer is : "+ maxInteger.maximum());
+        FindMaxUsingClass<Float> maxFloat = new FindMaxUsingClass<Float>(2.7f, 25.5f, 50.9f, 44.9f);
+        System.out.println("The maximum value between the three Float is : "+ maxFloat.maximum());
+        FindMaxUsingClass<String> maxString = new FindMaxUsingClass<String>("abc", "pug", "zca", "zza");
+        System.out.println("The maximum value between the three String is : "+ maxString.maximum());
+        }
 }
