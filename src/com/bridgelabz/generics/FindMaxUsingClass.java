f84a989d341d857eaea9b3ff4e5fb94a92a497e1
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-12-07 21:09:30 +0530

    Added:find maximum values using generic class with having more than three parameters

--- src/com/bridgelabz/generics/FindMaxUsingClass.java
+++ src/com/bridgelabz/generics/FindMaxUsingClass.java
@@ -1,33 +1,35 @@
 package com.bridgelabz.generics;
-
 public class FindMaxUsingClass<T extends Comparable <T>> {
     /**
-     * initialize instance variables
+     * initialize with more than three instance variables
      */
-    T number1, number2, number3;
+    T number1, number2, number3,number4;
 
     /**
      * parameterized constructor call
      */
-    public FindMaxUsingClass(T number1, T number2, T number3) {
+    public FindMaxUsingClass(T number1, T number2, T number3,T number4) {
         this.number1 = number1;
         this.number2 = number2;
         this.number3 = number3;
+        this.number4 = number4;
     }
     /**
      * static method maxOfValues by passing three instance variables
      */
-    public static <T extends Comparable <T>> T maxOfValues(T number1, T number2, T number3) {
+    public static <T extends Comparable <T>> T maxOfValues(T number1, T number2, T number3,T number4) {
         T max = number1;
         if (number2.compareTo(max) > 0)
             max = number2;
         if (number3.compareTo(max) > 0)
             max = number3;
+        if (number4.compareTo(max) > 0)
+            max = number4;
         return max;
     }
     public static void main(String[] args) {
-        System.out.println("The maximum value between the three Integer is : " + maxOfValues (2, 25, 50)+ "\n");
-        System.out.println("The maximum value between the three Float is : " + maxOfValues (2.7f, 25.5f, 50.9f)+ "\n");
-        System.out.println("The maximum value between the three String is : " + maxOfValues ("apple", "peach", "banana"));
+        System.out.println("The maximum value between the three Integer is : " + maxOfValues (2, 25, 50,65)+ "\n");
+        System.out.println("The maximum value between the three Float is : " + maxOfValues (2.7f, 25.5f, 50.9f,66.1f)+ "\n");
+        System.out.println("The maximum value between the three String is : " + maxOfValues ("apple", "peach", "banana","guava"));
     }
 }
